233a2919d81ff14cccd300acc882b0be5329b898
nanliha <nanlihao@gmail.com>
2018-07-17 14:01:58 +1000

    test

--- hello.py
+++ hello.py
@@ -1,3 +1,11 @@
+import ptvsd
+
+# Allow other computers to attach to ptvsd at this IP address and port, using the secret
+ptvsd.enable_attach("my_secret", address = ('1.2.3.4', 3000))
+
+# Pause the program until a remote debugger is attached
+ptvsd.wait_for_attach()
+
 import torch
-asdd
+print('hello world2')
 print('hello world')
